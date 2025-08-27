9e2534928520ebf9bcbf46b80aa9797e733f5d3b
Detroix23 <detroix23@gmail.com>
2025-08-27 15:56:03 +0000

    part2b body 2.

--- notes-frontend1/src/App.jsx
+++ notes-frontend1/src/App.jsx
@@ -4,6 +4,7 @@ import Note from './components/Note';
 const App = (props) => {
   const [ notes, setNotes ] = useState(props.notes);
   const [ newNote, setNewNote ] = useState("");
+  const [ showAll, setShowAll ] = useState(true);
 
   const handleNoteAdd = (event) => {
     event.preventDefault();
@@ -27,11 +28,22 @@ const App = (props) => {
     setNewNote(event.target.value);
   }
 
+  const switchNoteShow = () => setShowAll(!showAll);
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={switchNoteShow}>
+          Show {showAll ? 'important only' : 'all'}.
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
