6b599955b257679ae7f812923abe03820134a9bc
Detroix23 <detroix23@gmail.com>
2025-08-27 15:10:22 +0000

    ex2.5.

--- course_info/src/App.jsx
+++ course_info/src/App.jsx
@@ -1,37 +1,4 @@
-
-const CoursePart = ({ name, exercises }) => (
-  <li>
-    {name}: {exercises}
-  </li>
-);
-
-const CourseExercisesSum = ({ sum }) => (
-  <p>
-    <b>Total of {sum} exercises.</b>
-  </p>
-);
-
-
-const Course = ({ name, parts }) => {
-  // Already using reduce (ex2.3).
-  let exercises_sum = parts.reduce((sum, part) => sum + part.exercises, 0);
-  
-  console.log(`Comp.Course: ${name}, ex sum: ${exercises_sum}, with parts:`, parts);
-  
-  return (
-    <div>
-      <h2>{name}</h2>
-      <ul>
-        {parts.map((part) => <CoursePart key={part.id} name={part.name} exercises={part.exercises} />)}
-      </ul>
-      <CourseExercisesSum sum={exercises_sum}/>
-    </div>
-  );
-}; 
-
-const CourseList = ({ courses }) => courses.map((course) => (
-    <Course key={course.id} name={course.name} parts={course.parts} />
-  ));
+import { CourseList } from './components/Course';
 
 const App = () => {
   const courses = [
