7fdae906a494f8de5d788b79f6ef72d1175c9173
Detroix23 <detroix23@gmail.com>
2025-08-27 17:46:41 +0000

    ex2.6.

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,26 +1,70 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+const Person = ({ name }) => <li>{name}</li>
+
+const PersonsList = ({ persons }) => {
+  return (
+    <ul>
+      {persons.map((person) => 
+        <Person key={person.id} name={person.name} />
+      )}  
+    </ul>
+  );
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { 
+      name: 'Arto Hellas', 
+      id: 0
+    }
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const addPersonToPhonebook = (event) => {
+    event.preventDefault();
+
+    const personNames = persons.map((person) => person.name); 
+    const personAlreadyExists = personNames.includes(newName);
+    console.log(personAlreadyExists);
+    
+
+    // Add the person (or not if invalid entry).
+    if (newName !== '' && !personAlreadyExists) {
+      const id = persons[persons.length - 1].id + 1;
+      console.log(`Form.addPerson - Person: ${newName}, id: ${id}`);
+      const persons_new = persons.concat(
+        {
+          name: newName, 
+          id: id
+        }
+      );
+      setPersons(persons_new);
+    } else {
+      console.log(`Form.addPerson - Empty string or already exists (${newName}).`);
+    }
+  };
+
+  const handlePersonInput = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPersonToPhonebook} >
         <div>
-          name: <input />
+          name: 
+          <input type="text" onChange={handlePersonInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <PersonsList persons={persons} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
