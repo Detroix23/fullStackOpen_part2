6cb233205a3f606dc1f452bc560e07006bb4636a
Detroix23 <detroix23@gmail.com>
2025-08-27 19:17:41 +0000

    ex2.10-2.

--- phonebook/src/components/Persons.jsx
+++ phonebook/src/components/Persons.jsx
@@ -2,13 +2,19 @@
 const Person = ({ name, number }) => (
   <tr>
     <td>{name}</td>
-    <td>{number}</td>
+    <td>{number === '' ? "none" : number}</td>
   </tr>
 );
 
 const PersonsList = ({ persons }) => {
   return (
     <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Number</th>
+        </tr>
+      </thead>
       <tbody>
         {persons.map((person) => 
           <Person key={person.id} name={person.name} number={person.number}/>
